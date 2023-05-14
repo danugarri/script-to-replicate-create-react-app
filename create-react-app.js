240a769226577c1e9628ee8770a16435ee22143f
danugarri <danugarri@hotmail.com>
2023-05-14 21:51:01 +0200

    added config files to the script

--- create-react-app.js
+++ create-react-app.js
@@ -106,6 +106,94 @@ fs.writeFileSync(
 # Get the app ready for production
 - npm run build`
 );
+// config files
+fs.writeFileSync(
+  path.join(projectPath, ".gitignore"),
+  `# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
+
+# dependencies
+/node_modules
+/.pnp
+.pnp.js
+
+# testing
+/coverage
+
+# production
+/build
+
+# misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*`
+);
+
+fs.writeFileSync(
+  path.join(projectPath, "package.json"),
+  `
+  {
+    "name": "danugarri_create-react-app",
+    "author": "danugarri",
+    "version": "0.1.0",
+    "private": true,
+    "dependencies": {
+      "@testing-library/jest-dom": "^5.16.1",
+      "@testing-library/react": "^12.1.2",
+      "@testing-library/user-event": "^13.5.0",
+      "react": "^18.0.0",
+      "react-dom": "^18.0.0",
+      "react-scripts": "5.0.1",
+      "web-vitals": "^2.1.3"
+    },
+    "scripts": {
+      "start": "react-scripts start",
+      "build": "react-scripts build",
+      "test": "react-scripts test",
+      "eject": "react-scripts eject"
+    },
+    "eslintConfig": {
+      "extends": [
+        "react-app",
+        "react-app/jest"
+      ]
+    },
+    "browserslist": {
+      "production": [
+        ">0.2%",
+        "not dead",
+        "not op_mini all"
+      ],
+      "development": [
+        "last 1 chrome version",
+        "last 1 firefox version",
+        "last 1 safari version"
+      ]
+    },
+    "devDependencies": {
+      "prettier": "^2.6.2"
+  }
+  }
+  `
+);
+fs.writeFileSync(
+  path.join(projectPath, ".prettierrc"),
+  `
+{
+  "tabWidth": 4,
+  "useTabs": false,
+  "semi": true,
+  "bracketSpacing": true,
+  "jsxSingleQuote": false,
+  "singleQuote": true
+}
+`
+);
 
 // This will have to wait
 // Done!
